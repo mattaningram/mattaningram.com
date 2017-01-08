dc0d97b50f9f58aa3d4229cfd247bf36ed5d6fbb
Mattan Ingram <mattaningram@gmail.com>
2017-01-08 22:23:10 +1100

    switch to jekyll structure

--- js/app.js
+++
@@ -1,6 +0,0 @@
-angular.module('website', ['website.controllers', 'website.directives', 'website.services', 'website.filters']);
-
-angular.module('website.controllers', []);
-angular.module('website.directives', []);
-angular.module('website.services', []);
-angular.module('website.filters', []);
--- js/directives/redBlueM.js
+++
@@ -1,8 +0,0 @@
-angular.module('website.directives')
-  .directive('redBlueM', [
-    function () {
-      return {
-        replace: true,
-        templateUrl: 'views/red-blue-m.html'
-      }
-    }]);
