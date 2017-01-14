401f9e51af9f8e872c9d462a2f04384471f901a0
Mattan Ingram <mattaningram@gmail.com>
2017-01-14 21:09:20 +1100

    setting up page layout

--- _work/canvs.md
+++
@@ -1,9 +0,0 @@
----
-title: Canvs
-year: 2011 to 2016
-logo: canvs_logo_white.svg
----
-
-### Canvs Page
-
-Canvs content.
--- js/load-m-logo.js
+++ js/load-m-logo.js
@@ -1,6 +1,6 @@
 function generateRandomM() {
     var min = 1, max = 2;
-    $(".m-logo-wrap").load("mlogos/" + Math.floor (Math.random() * (max - min + 1) + min) + ".html");
+    $(".m-logo-wrap").load("/mlogos/" + Math.floor (Math.random() * (max - min + 1) + min) + ".html");
 }
 
 $(document).ready(function() {
