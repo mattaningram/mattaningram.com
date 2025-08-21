298431e91d7dc0298015b7ada1a490164578e9dd
Mattan Ingram <mattaningram@gmail.com>
2025-08-21 16:57:14 -0400

    Fixing astro styles and other issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mattaningram.com',
-  output: 'static',
-  build: {
-    assets: 'assets'
-  },
-  vite: {
-    css: {
-      devSourcemap: true
-    }
-  }
-});
+	site: 'https://mattaningram.com',
+	vite: {
+		css: {
+			devSourcemap: true,
+		},
+	},
+})
