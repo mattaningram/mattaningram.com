8f6918758d5b6d07e1f965686bedacf294d6b186
Mattan Ingram <mattaningram@gmail.com>
2025-08-29 17:53:28 -0400

    Add header footer and icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from 'astro/config'
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://mattaningram.com',
-	vite: {
-		css: {
-			devSourcemap: true,
-		},
+  site: 'https://mattaningram.com',
+
+  vite: {
+      css: {
+          devSourcemap: true,
+      },
 	},
+
+  integrations: [icon()],
 })
