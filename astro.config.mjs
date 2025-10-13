1d55d60327c4987b9fd633f965443f7ae72c5a1f
Mattan Ingram <mattaningram@gmail.com>
2025-10-12 20:01:35 -0400

    Revel update and style improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,20 @@
 import { defineConfig } from 'astro/config'
 
-import icon from 'astro-icon';
+import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mattaningram.com',
+	site: 'https://mattaningram.com',
 
-  vite: {
-      css: {
-          devSourcemap: true,
-      },
+	image: {
+		responsiveStyles: true,
 	},
 
-  integrations: [icon()],
+	vite: {
+		css: {
+			devSourcemap: true,
+		},
+	},
+
+	integrations: [icon()],
 })
