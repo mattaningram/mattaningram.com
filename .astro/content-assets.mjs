7e029f9708881378571a3bac64473dd4d98d2b47
Mattan Ingram <mattaningram@gmail.com>
2026-03-15 15:54:47 -0400

    Delete content-assets.mjs

--- .astro/content-assets.mjs
+++
@@ -1,7 +0,0 @@
-
-import __ASTRO_IMAGE_IMPORT_ZSc0uk from "../../logos/metme_icon.png?astroContentImageFlag=&importer=src%2Fcontent%2Fresume%2F01-metme.md";
-import __ASTRO_IMAGE_IMPORT_ZRTKa1 from "../../logos/animxyz-icon.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fresume%2F05-animxyz.md";
-import __ASTRO_IMAGE_IMPORT_Zm1Su2 from "../../logos/visible_icon.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fresume%2F03-visible.md";
-import __ASTRO_IMAGE_IMPORT_cYBbS from "../../logos/yaherd_icon.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fresume%2F06-yaherd.md";
-export default new Map([["../../logos/metme_icon.png?astroContentImageFlag=&importer=src%2Fcontent%2Fresume%2F01-metme.md", __ASTRO_IMAGE_IMPORT_ZSc0uk], ["../../logos/animxyz-icon.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fresume%2F05-animxyz.md", __ASTRO_IMAGE_IMPORT_ZRTKa1], ["../../logos/visible_icon.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fresume%2F03-visible.md", __ASTRO_IMAGE_IMPORT_Zm1Su2], ["../../logos/yaherd_icon.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fresume%2F06-yaherd.md", __ASTRO_IMAGE_IMPORT_cYBbS]]);
-		
